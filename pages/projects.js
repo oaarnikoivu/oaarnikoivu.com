e88a8cbd864937ac1b12ee1c0e20b476d6e42edc
Oliver Aarnikoivu <oliveraarnikoivu@icloud.com>
2023-04-22 20:13:08 +0200

    feat: projects page setup

--- pages/projects.js
+++ pages/projects.js
@@ -1,3 +1,21 @@
-export default function Projects() {
-  return <h1 className="text-2xl">Projects</h1>;
+export default function Projects({ projects }) {
+  return (
+    <>
+      <h1 className="text-2xl">Projects</h1>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://api.github.com/users/oaarnikoivu/repos");
+  let projects = await res.json();
+  projects = projects
+    .filter((project) => !!project.description)
+    .sort((a, b) => b.stargazers_count - a.stargazers_count);
+
+  return {
+    props: {
+      projects,
+    },
+  };
 }
