d9b9e4a39f0cd7abfddd1eb04824c61a1dbdfc5f
Oliver Aarnikoivu <oliveraarnikoivu@icloud.com>
2023-04-23 13:54:01 +0200

    feat: finalize first version

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <section>
       <h1 className="text-2xl">About Me</h1>
-      <p className="mt-8 text-sm text-zinc-300">
+      <p className="mt-8">
         My name is Oliver Aarnikoivu. I was born in Finland, and raised in
         Luxembourg. I completed my Bachelors of Computer Science at The Robert
         Gordon University in Aberdeen, Scotland, followed by a Masters in
@@ -17,24 +17,24 @@ export default function About() {
         , where I am actively involved in enhancing the design, experience, and
         performance of the product.
       </p>
-      <p className="mt-4 text-sm text-zinc-300">
+      <p className="mt-6">
         I am interested in a wide range of topics, including Software
         Engineering, Design, Human Computer Interaction, and Artificial
         Intelligence.
       </p>
-      <p className="mt-4 text-sm text-zinc-300">
+      <p className="mt-6">
         In my spare time I enjoy tinkering with new technologies, flaneuring,
         going on long walks, spending time with friends, reading up on random
         topics that I find interesting, and occasionally, lifting weights.
       </p>
-      <p className="mt-4 text-sm text-zinc-300">
+      <p className="mt-6">
         This website is primarily used to showcase my experiments with new tools
         & technologies. From time to time, I may express my thoughts and write
         about something random related to my interests, which you can find under
         the <UnderlineLink href="/blog" text="Blog" /> page.
       </p>
 
-      <p className="mt-4 text-sm text-zinc-300">
+      <p className="mt-6">
         Thanks for stopping by, and I hope you find something that piques your
         interest.
       </p>
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-start gap-8 items-start">
         <Image
-          alt=""
-          src="https://avatars.githubusercontent.com/u/22493428?s=400&u=da52e73af477adecb6fd39efdeda0e4b85860fc2&v=4"
-          width={140}
-          height={140}
           className="rounded-full grayscale"
+          alt="profile"
+          src="https://avatars.githubusercontent.com/u/22493428?s=400&u=da52e73af477adecb6fd39efdeda0e4b85860fc2&v=4"
+          width={100}
+          height={100}
         />
         <div>
           <h1 className="text-2xl">Oliver Aarnikoivu</h1>
-          <p className="mt-4 text-zinc-200 text-sm">
+          <p className="mt-4">
             Hey, I'm Oliver. Born in Finland, raised in Luxembourg. Full Stack
             Software Engineer at{" "}
             <UnderlineLink
