bb2383877892491b368644fd9ba766bf6529444a
Oliver Aarnikoivu <oliveraarnikoivu@icloud.com>
2023-05-18 17:19:34 +0200

    chore: clear git cache

--- components/UnderlineLink.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function UnderlineLink({ href, text }) {
-  return (
-    <Link href={href} target="_blank" className="underline hover:text-white">
-      {text}
-    </Link>
-  );
-}
